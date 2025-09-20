cee8567c0d139112a7b266e85ce82554b85e0a16
Ian Tuitoek <iantuitoek4@gmail.com>
2025-09-20 09:58:05 +0300

    Implement return_evens and make_exclamation functions

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [n for n in num_list if n%2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + "!" for sentence in sentence_list]
+
+
+print(return_evens([0,1,3,5,7,8,9]))
